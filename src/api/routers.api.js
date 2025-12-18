dbca871cb2dfd4ff608ba6687e28c162a46bee5a
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-18 17:24:47 +0530

    feat: remove groupRouter from API routes

--- src/api/routers.api.js
+++ src/api/routers.api.js
@@ -2,4 +2,3 @@
 export { default as healthCheckRouter } from './v1/healthcheck/healthcheck.routes.js';
 export { default as cohortRouter } from './v1/cohort/cohort.routes.js';
 export { default as authRouter } from './v1/auth/auth.routes.js';
-export { default as groupRouter } from './v1/group/group.routes.js';
