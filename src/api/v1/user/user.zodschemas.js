2dbfb36b439a868905aeb031771215b7a01e65c6
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2025-12-24 12:05:35 +0530

    fix: improve validation error messages in user role and profile schemas

--- src/api/v1/user/user.zodschemas.js
+++ src/api/v1/user/user.zodschemas.js
@@ -7,7 +7,7 @@ import { z } from 'zod';
 // zod schema for update user role
 export const updateUserRoleSchema = z.object({
   body: z.object({
-    userEmail: z.email({ error: 'userEmail is required' }).trim().toLowerCase(),
+    userEmail: z.email({ error: 'Valid userEmail is required' }).trim().toLowerCase(),
     newRole: z.enum(availableUserRoles),
   }),
 });
@@ -18,24 +18,30 @@ export const updateUserProfileSchema = z.object({
     newUserExpertise: z
       .array(
         z.object({
-          roleName: z.string().min(1, { error: 'roleName is required' }).trim(),
+          roleName: z.string().nonempty({ error: 'Atleast one roleName is required' }).trim(),
           techStack: z
             .array(
               z.object({
-                skillName: z.string().min(1, { error: 'skillName is required' }).trim(),
+                skillName: z
+                  .string()
+                  .nonempty({ error: 'Atleast one skillName is required' })
+                  .trim(),
                 experienceInMonths: z
                   .number()
                   .min(1, { error: 'experienceInMonths must be at least 1' }),
               })
             )
-            .min(1, { error: 'At least one techStack is required' }),
+            .nonempty({ error: 'At least one techStack is required' }),
         })
       )
       .optional(),
     newSocialLinks: z
       .array(
         z.object({
-          platformName: z.string().min(1, { error: 'platformName is required' }).trim(),
+          platformName: z
+            .string()
+            .nonempty({ error: 'Atleast one platformName is required' })
+            .trim(),
           platformURL: z.url({ error: 'Valid platformURL is required' }).trim(),
         })
       )
