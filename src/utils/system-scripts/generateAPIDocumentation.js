31f6a6b704f8c077dfbd7a27e9a1c999682a2902
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-01 14:58:55 +0530

    feat: refactor API documentation generation script for improved structure and error handling

--- src/utils/system-scripts/generateAPIDocumentation.js
+++ src/utils/system-scripts/generateAPIDocumentation.js
@@ -1,26 +1,55 @@
 // import external modules
 import swaggerJsdoc from 'swagger-jsdoc';
-import fs from 'fs';
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-(function () {
-  // generate API documentation in OpenAPI 3.0 format using swagger-jsdoc
-  const generatedAPIDocs = swaggerJsdoc({
-    swaggerDefinition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'ChaiHub-Server API Documentation',
-        description: 'Interactive API reference for ChaiHub-Server',
-        version: '1.0.0',
-        contact: {
-          name: 'Manjeet Singh',
-          url: 'https://github.com/ManjeetSingh-02',
-          email: 'manjeetsingh.wrk@gmail.com',
+(async function () {
+  const rootDir = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../../');
+  const apiFilesPatterns = ['src/api/v1/**/*.controllers.js'];
+  const outputFile = path.join(rootDir, 'docs/apiDocs.json');
+
+  try {
+    // generate API documentation in OpenAPI 3.0 format using swagger-jsdoc
+    const generatedAPIDocs = swaggerJsdoc({
+      definition: {
+        openapi: '3.0.0',
+        info: {
+          title: 'ChaiHub-Server API Documentation',
+          description: 'Interactive API reference for ChaiHub-Server',
+          version: '1.0.0',
+          contact: {
+            name: 'Manjeet Singh',
+            url: 'https://github.com/ManjeetSingh-02',
+            email: 'manjeetsingh.wrk@gmail.com',
+          },
         },
+        servers: [
+          {
+            url: 'http://localhost:3000',
+            description: 'Development Server',
+          },
+        ],
       },
-    },
-    apis: ['./src/modules/**/*.js'],
-  });
+      apis: apiFilesPatterns,
+    });
 
-  // save the generated documentation to a JSON file in the docs directory
-  fs.writeFileSync('./docs/apiDocumentation.json', JSON.stringify(generatedAPIDocs, null, 2));
+    // save the generated documentation to a JSON file in the output directory
+    await fs.writeFile(outputFile, JSON.stringify(generatedAPIDocs, null, 2), 'utf8');
+    console.log('--- API Documentation JSON Stored in Output File: ✅');
+  } catch (error) {
+    // log error
+    console.error('---------------------------------------------------------');
+    console.error('ERROR DURING API DOCS JSON GENERATION');
+    console.error(`ERROR DETAILS: ${error.message}`);
+    console.error('RUN THE SCRIPT AGAIN AFTER FIXING THE ISSUE');
+    console.error('---------------------------------------------------------');
+
+    // delete the output file if it was partially created
+    await fs.unlink(outputFile).catch(() => {});
+    console.log('--- Partially Created Output File Deletion: ✅');
+
+    // exit with failure
+    process.exit(1);
+  }
 })();
