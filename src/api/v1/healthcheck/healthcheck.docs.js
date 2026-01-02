7835b590e9d5c3fdfaf98ec45279fd1fe8d2b2a3
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-01-02 20:34:40 +0530

    feat: remove unnecessary security tag from health check documentation

--- src/api/v1/healthcheck/healthcheck.docs.js
+++ src/api/v1/healthcheck/healthcheck.docs.js
@@ -3,7 +3,6 @@
  * @operationId healthCheck
  * @summary Check Server Health
  * @tags HealthCheck - Health Check Endpoints
- * @security
  * @description Endpoint to check the status of the server.
  * @return {APISuccessResponse} 200 - Successful response
  * @example response - 200 - Successful health check response
