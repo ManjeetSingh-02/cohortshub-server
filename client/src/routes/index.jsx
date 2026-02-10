43e5fc1c214e090bdf0d61eaa73d618445b14594
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-02-11 03:29:30 +0530

    feat: implement token refresh logic in route beforeLoad

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { axiosInstance } from '@/lib/axios';
+import { useAuthStore } from '@/stores/auth.store';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
-});
+  beforeLoad: async () => {
+    // get state and action from auth store
+    const { accessToken, updateAccessToken } = useAuthStore.getState();
 
-function RouteComponent() {
-  return <div>Hello "/"!</div>;
-}
+    // if accessToken exists, redirect to the cohorts page
+    if (accessToken) throw redirect({ to: '/cohorts' });
+
+    // else make request to refresh token, update in store and redirect to cohorts page
+    try {
+      const data = await axiosInstance.patch('/auth/token/refresh');
+      updateAccessToken(data.response.data.accessToken);
+      throw redirect({ to: '/cohorts' });
+    } catch (error) {
+      throw redirect({ to: '/login' });
+    }
+  },
+});
