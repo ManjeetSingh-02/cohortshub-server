86a2a073b18393ec24e411b58fbc57b3289a0366
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-02-11 03:27:25 +0530

    feat: add notFoundComponent to root route

--- client/src/routes/__root.jsx
+++ client/src/routes/__root.jsx
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { Outlet, createRootRoute } from '@tanstack/react-router';
 
 export const Route = createRootRoute({
+  notFoundComponent: () => (
+    <div className="flex min-h-screen items-center justify-center">Not Found</div>
+  ),
   component: RootComponent,
 });
 
