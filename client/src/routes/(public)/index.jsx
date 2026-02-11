c100fe6bbcf499d68840deddde51f78a4b54529d
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-02-11 18:08:13 +0530

    feat: simplify public route by removing authentication checks and redirect logic

--- client/src/routes/(public)/index.jsx
+++ client/src/routes/(public)/index.jsx
@@ -1,22 +1,9 @@
-import { axiosInstance } from '@/lib/axios';
-import { useAuthStore } from '@/stores/auth.store';
-import { createFileRoute, redirect } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/(public)/')({
-  beforeLoad: async () => {
-    // get state and action from auth store
-    const { accessToken, updateAccessToken } = useAuthStore.getState();
-
-    // if accessToken exists, redirect to the cohorts page
-    if (accessToken) throw redirect({ to: '/cohorts' });
-
-    // else make request to refresh token, update in store and redirect to cohorts page
-    try {
-      const data = await axiosInstance.patch('/auth/token/refresh');
-      updateAccessToken(data.response.data.accessToken);
-      throw redirect({ to: '/cohorts' });
-    } catch (error) {
-      throw redirect({ to: '/login' });
-    }
-  },
+  component: RouteComponent,
 });
+
+function RouteComponent() {
+  return <div>Coming Soon</div>;
+}
