0edd8860f60895b33b273177217c390e9090691d
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-02-11 16:52:51 +0530

    feat: update beforeLoad to use async/await for restoreSession

--- client/src/routes/(public)/login/route.jsx
+++ client/src/routes/(public)/login/route.jsx
@@ -2,8 +2,8 @@ import { restoreSession } from '@/lib/restoreSession';
 import { createFileRoute, redirect } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/(public)/login')({
-  beforeLoad: () => {
-    const isAuthenticated = restoreSession();
+  beforeLoad: async () => {
+    const isAuthenticated = await restoreSession();
 
     // if the user is already authenticated, redirect to the cohorts page
     if (isAuthenticated) throw redirect({ to: '/cohorts' });
