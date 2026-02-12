610b9800ffaebccaaf447cad946a65ad9d3c78d1
ManjeetSingh-02 <manjeetsingh.wrk@gmail.com>
2026-02-12 13:33:19 +0530

    refactor: simplify logoutUser function

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,17 +1,10 @@
 import { axiosInstance } from '@/lib/axios';
 import { envConfig } from '@/lib/env';
-import { useAuthStore } from '@/stores/auth.store';
 
 export const authService = {
-  // function to login user with google
+  // service to login user with google
   loginWithGoogle: () => (window.location.href = `${envConfig.VITE_API_URL}/auth/login/google`),
 
-  // function to logout user
-  logoutUser: async () => {
-    try {
-      await axiosInstance.post('/auth/logout');
-    } finally {
-      useAuthStore.getState().clearAccessToken();
-    }
-  },
+  // service to logout user
+  logoutUser: () => axiosInstance.post('/auth/logout'),
 };
